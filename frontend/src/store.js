794102a200cd5fe4c3b554b394f092cebe55b521
rafaelmorales <rfalmorales@gmail.com>
2021-05-28 23:47:10 -0400

    Commit Cart

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,12 +5,20 @@ import {
   productDetailsReducers,
   productReducers,
 } from './reducers/productReducers';
+import { cartReducers } from './reducers/cartReducers';
 
 const reducer = combineReducers({
   productList: productReducers,
   productDetails: productDetailsReducers,
+  cart: cartReducers,
 });
-const initialState = {};
+
+const cartItemsFromStorage = localStorage.getItem('cartItems')
+  ? JSON.parse(localStorage.getItem('cartItems'))
+  : [];
+const initialState = {
+  cart: cartItemsFromStorage,
+};
 const middleware = [thunk];
 const store = createStore(
   reducer,
